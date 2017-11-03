92d4694ff72d4653a215aad150d58f7b2e3a347e
Oleg Demchuk <demchukoleg88@gmail.com>
2017-11-03 18:08:33 +0200

    Setup routes for photos and categories

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { getPhotos } from './photos';
+import { getPhotos, createPhoto, editPhoto, deletePhoto } from './photos';
 import {
   getSessions,
   getSessionById,
@@ -8,10 +8,21 @@ import {
   editSession,
   deleteSession,
 } from './sessions';
+import {
+  getCategories,
+  getCategoryById,
+  createCategory,
+  editCategory,
+  deleteCategory,
+} from './categories';
 
 const router = Router();
 
-router.get('/api/photos', getPhotos);
+router
+  .get('/api/photos', getPhotos)
+  .post('/api/photos', createPhoto)
+  .put('/api/photos/:id', editPhoto)
+  .delete('/api/photos/:id', deletePhoto);
 
 router
   .get('/api/sessions', getSessions)
@@ -19,5 +30,12 @@ router
   .post('/api/sessions', createSession)
   .put('/api/sessions/:id', editSession)
   .delete('/api/sessions/:id', deleteSession);
+
+router
+  .get('/api/categories', getCategories)
+  .get('/api/categories/:id', getCategoryById)
+  .post('/api/categories', createCategory)
+  .put('/api/categories/:id', editCategory)
+  .delete('/api/categories/:id', deleteCategory);
 
 export default router;
