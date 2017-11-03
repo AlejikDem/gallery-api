5da38c6d9e5b392cca37d39319ab968b2400b71a
Oleg Demchuk <demchukoleg88@gmail.com>
2017-11-03 12:54:42 +0200

    Add base server with db

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import config from './config';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+mongoose.Promise = global.Promise;
+mongoose.connection.openUri(config.db);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
+
+module.exports = app;
