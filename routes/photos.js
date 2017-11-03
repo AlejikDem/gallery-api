2c76e99acc8ec71fca701df2a2844097c7b1bde2
Oleg Demchuk <demchukoleg88@gmail.com>
2017-11-03 18:05:16 +0200

    Add scaffold for photos endpoints

--- routes/photos.js
+++ routes/photos.js
@@ -5,3 +5,28 @@ export const getPhotos = (req, res) => {
     .then(photos => res.json(photos))
     .catch(err => res.send(err));
 };
+
+export const createPhoto = (req, res) => {
+  const photo = new Photo(req.body);
+
+  photo
+    .save(req.body)
+    .then(data => res.send(data))
+    .catch(err => res.send(err));
+};
+
+export const editPhoto = (req, res) => {
+  Photo.findById(req.params.id)
+    .then(data => {
+      const newPhoto = Object.assign(data, req.body);
+
+      newPhoto.save().then(data => res.send(data));
+    })
+    .catch(err => res.send(err));
+};
+
+export const deletePhoto = (req, res) => {
+  Category.findOneAndRemove({ _id: req.params.id })
+    .then(data => res.send(data))
+    .catch(err => res.send(err));
+};
