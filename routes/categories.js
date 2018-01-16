5b6f5d1547d0e902e352de9e04689a3f97499af7
Oleg Demchuk <demchukoleg88@gmail.com>
2018-01-16 16:13:15 +0200

    Other categories methods refac

--- routes/categories.js
+++ routes/categories.js
@@ -11,28 +11,43 @@ export const getCategories = async (req, res) => {
 };
 
 export const getCategoryById = async (req, res) => {
-  const category = await Category.findById(req.params.id);
-  const photos = await Photo.find({ category: category._id }).populate(
-    'session',
-  );
+  try {
+    const category = await Category.findById(req.params.id);
+    const photos = await Photo.find({ category: category._id }).populate(
+      'session',
+    );
 
-  res.send({ instance: category, photos });
+    res.send({ instance: category, photos });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-export const createCategory = (req, res) => {
-  Category.create(req.body)
-    .then(data => res.send(data))
-    .catch(err => res.send(err));
+export const createCategory = async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.send(category);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-export const editCategory = (req, res) => {
-  Category.findByIdAndUpdate(req.params.id, req.body)
-    .then(data => res.send(data))
-    .catch(err => res.send(err));
+export const editCategory = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body);
+    res.send(category);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-export const deleteCategory = (req, res) => {
-  Category.findOneAndRemove({ _id: req.params.id })
-    .then(data => res.send({ message: 'Category has been deleted' }))
-    .catch(err => res.send(err));
+export const deleteCategory = async (req, res) => {
+  try {
+    await Category.findOneAndRemove({ _id: req.params.id });
+    await Photo.update({ category: req.params.id }, { category: null });
+
+    res.send({ message: 'Category has been deleted' });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
